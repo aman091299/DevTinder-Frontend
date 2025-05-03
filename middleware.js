9c00e145caa160bcceccec4b0edeed2f64e2aa21
aman <aman091299@gmail.com>
2025-05-03 13:23:27 +0530

    adding checking token is comming in middle ware or not

--- middleware.js
+++ middleware.js
@@ -4,9 +4,10 @@ import { NextResponse } from 'next/server'
 const protectedRoutes=['/','/profile','/connections','/request'];
 
 export const middleware=async (req)=>{
-    const path = req.nextUrl.pathname;
-
+    const path = req?.nextUrl?.pathname;
+   console.log('path',req?.nextUrl?.pathname);
     const token = req.cookies.get('token')?.value;
+    console.log('token',token);
   
     if(protectedRoutes.includes(path) && ! token ){
         return NextResponse.redirect(new URL('/login', req.nextUrl));
